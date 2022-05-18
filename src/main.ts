360ded8d574ef49840817f6b114065c0c533ea35
Zouhair <zouhair.rouika@gfinity.net>
2022-05-18 18:35:24 +0100

    Deps

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
+import Naive from 'naive-ui'
 import App from './App.vue'
 import generatedRoutes from '~pages'
 
-import '@unocss/reset/tailwind.css'
+import './styles/reset.css'
 import './styles/main.css'
 import 'uno.css'
 
@@ -15,6 +16,7 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
     // install all modules under `modules/`
+    ctx.app.use(Naive)
     Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
   },
 )
